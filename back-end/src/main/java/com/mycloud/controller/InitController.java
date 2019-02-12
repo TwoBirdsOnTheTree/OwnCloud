0b9f019fe80c38b8cbe188c864295e3a0cc6fa1e
784925761@qq.com <2smqyV3CwgQb>
2019-02-12 18:47:05 +0800

    添加备注

--- back-end/src/main/java/com/mycloud/controller/InitController.java
+++ back-end/src/main/java/com/mycloud/controller/InitController.java
@@ -11,7 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class InitController extends BaseController {
 
     public void ss() {
-        // 执行db文件
+        // 在项目根文件夹下创建配置文件夹
+        // 1. 首先需要判断是否已经创建了该文件夹
+        // 2. 这个创建的文件夹不应该在页面中显示
+
+        // 在配置文件夹下创建db文件
+
+        // 获取sql文件, 执行sql
     }
 
 }
