d42cffcf5f0b36b3862f2fcffe0f4750013edeca
784925761@qq.com <2smqyV3CwgQb>
2019-02-14 20:00:03 +0800

    下载功能

--- back-end/src/main/java/com/mycloud/controller/FileController.java
+++ back-end/src/main/java/com/mycloud/controller/FileController.java
@@ -37,7 +37,7 @@ public class FileController {
             }
             // 判断是文件下载还是文件夹下载
             if (file.isFile()) {
-                downloadFile(request, response);
+                downloadFile(request, response, file);
             } else if (file.isDirectory()) {
 
             }
@@ -50,24 +50,49 @@ public class FileController {
         }
     }
 
-    private void downloadFile(HttpServletRequest request, HttpServletResponse response) {
+    private void downloadFile(HttpServletRequest request, HttpServletResponse response, File downloadFile) throws Exception {
+        //
+        String contentType = request.getServletContext().getMimeType(downloadFile.getName());
+        // 设置响应头
+        // 参考资料：https://www.ibm.com/developerworks/cn/java/joy-down/index.html
+        // 坑爹地方一：看代码
+        response.setHeader("Accept-Ranges", "bytes");
+        // 坑爹地方二：http状态码要为206
+        response.setStatus(206);
+        response.setContentType(contentType);
+        response.setHeader("Content-Type", contentType);
+        // 这里文件名换你想要的，inline表示浏览器直接实用（我方便测试用的）
+        // 参考资料：http://hw1287789687.iteye.com/blog/2188500
+        // response.setHeader("Content-Disposition", "inline;filename=test.mp3");
         // 断点续传
         // Range: bytes=40-100 ：第40个字节到第100个字节之间的数据
         String range = request.getHeader("Range");
-        if (CommonUtil.isStringNotEmpty(range)) {
+        boolean rangeDownload = CommonUtil.isStringNotEmpty(range) && range.contains("bytes=");
+        if (rangeDownload) {
+            // range去掉头部的: bytes=
+            range = range.substring("bytes=".length());
             // 获取开始/结束下载位置
             String[] ranges = range.split("-");
-            String startByte = null;
-            String endByte = null;
+            String startByte = "0";
+            String endByte = Long.valueOf(downloadFile.length()).toString();
             if (ranges.length == 1) {
                 if (range.startsWith("-")) {
-                    endByte = ranges[1];
+                    endByte = ranges[0];
                 } else if (range.endsWith("-")) {
                     startByte = ranges[0];
                 }
             } else if (ranges.length == 2) {
-
+                startByte = ranges[0];
+                endByte = ranges[1];
             }
+            //要下载的长度 (为什么要+1?)
+            long contentLength = Long.valueOf(endByte) - Long.valueOf(startByte) + 1;
+            // 断点续传设置响应头
+            response.setHeader("Content-Length", String.valueOf(contentLength));
+            //坑爹地方三：Content-Range，格式为
+            // [要下载的开始位置]-[结束位置]/[文件总大小]
+            response.setHeader("Content-Range",
+                    String.format("bytes %s-%s/%s", startByte, endByte, downloadFile.lastModified()));
         }
     }
 }
