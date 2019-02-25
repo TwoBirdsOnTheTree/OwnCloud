2becbbae31542e6cb2d43790ce74c28f9a79de64
784925761@qq.com <2smqyV3CwgQb>
2019-02-25 16:19:44 +0800

    简易显示, 触发下载

--- front-end/src/views/Disk.vue
+++ front-end/src/views/Disk.vue
@@ -1,6 +1,19 @@
 <template>
     <div>
-        {{test}}
+        <table>
+            <tr>
+                <td>
+                    文件名
+                </td>
+                <td>操作</td>
+            </tr>
+            <tr v-if="null != test"
+                v-for="file of test"
+            >
+                <td>{{file.fileName}}</td>
+                <td @click="download(file)">下载</td>
+            </tr>
+        </table>
     </div>
 </template>
 
@@ -15,9 +28,17 @@
         created() {
             this.$axios.get('file/fileList?filePath=chat')
                 .then((response) => {
-                    this.test = response;
-                    console.log(response);
+                    this.test = response.data;
                 });
+        },
+        methods: {
+            download(file) {
+                console.log(file);
+                let filePath = file.filePath;
+                // 将字符\替换为/, 否则请求报错
+                filePath = String(filePath).replace(/\\/g, '/');
+                window.open('http://localhost:8188/file/download?filePath=' + filePath);
+            }
         }
     }
 </script>
