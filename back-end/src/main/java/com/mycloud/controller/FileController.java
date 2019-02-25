4b126769cb8b91af9ed0abcb3e33c93360b929d5
784925761@qq.com <2smqyV3CwgQb>
2019-02-25 12:00:30 +0800

    解决谷歌浏览器下载文件时显示"文件不存在"

--- back-end/src/main/java/com/mycloud/controller/FileController.java
+++ back-end/src/main/java/com/mycloud/controller/FileController.java
@@ -60,16 +60,14 @@ public class FileController {
     }
 
     private void downloadFile(HttpServletRequest request, HttpServletResponse response, File downloadFile) throws Exception {
-        // 文件目录
-        File music = downloadFile;
-        System.out.println("文件是否可读: " + music.canRead());
-        String range = request.getHeader("Range");
+        // System.out.println("文件是否可读: " + downloadFile.canRead());
 
         // 开始下载位置
         long startByte = 0;
         // 结束下载位置
-        long endByte = music.length() - 1;
-
+        long endByte = downloadFile.length() - 1;
+        // 是否时断电续传
+        String range = request.getHeader("Range");
         boolean rangeBoolean = range != null && range.contains("bytes=") && range.contains("-");
         // 有range的话
         if (rangeBoolean) {
@@ -95,52 +93,59 @@ public class FileController {
 
             } catch (NumberFormatException e) {
                 startByte = 0;
-                endByte = music.length() - 1;
+                endByte = downloadFile.length() - 1;
             }
         }
 
         // 要下载的长度（为啥要加一问小学数学老师去）
         long contentLength = endByte - startByte + 1;
         // 文件名
-        String fileName = music.getName();
+        String fileName = downloadFile.getName();
         // 文件类型
         String contentType = request.getServletContext().getMimeType(fileName);
 
         // 各种响应头设置
         // 参考资料：https://www.ibm.com/developerworks/cn/java/joy-down/index.html
         // 坑爹地方一：看代码
-        if (rangeBoolean)
+        if (rangeBoolean) {
             response.setHeader("Accept-Ranges", "bytes");
-        // 坑爹地方二：http状态码要为206
-        response.setStatus(HttpServletResponse.SC_PARTIAL_CONTENT);
+            // 坑爹地方二：http状态码要为206
+            // 谷歌浏览器设置206后, 下载时提示"文件不存在" (IE下无论是否设置206都正常)
+            //TODO 测试谷歌浏览器range下设置206是否正常
+            response.setStatus(HttpServletResponse.SC_PARTIAL_CONTENT);
+        }
         response.setContentType(contentType);
         response.setHeader("Content-Type", contentType);
         // 这里文件名换你想要的，inline表示浏览器直接实用（我方便测试用的）
         // 参考资料：http://hw1287789687.iteye.com/blog/2188500
-        response.setHeader("Content-Disposition", "attachment;filename=\"Untitled-5.txt\"");
+        String contentDisposition = String.format("attachment;filename=\"%s\"", downloadFile.getName());
+        response.setHeader("Content-Disposition", contentDisposition);
         response.setHeader("Content-Length", String.valueOf(contentLength));
         // 坑爹地方三：Content-Range，格式为
         // [要下载的开始位置]-[结束位置]/[文件总大小]
-        if (rangeBoolean)
-            response.setHeader("Content-Range", "bytes " + startByte + "-" + endByte + "/" + music.length());
+        if (rangeBoolean) {
+            String contentRange = String.format("bytes %s-%s/%s", startByte, endByte, downloadFile.length());
+            response.setHeader("Content-Range", contentRange);
+        }
         // 下载
         BufferedOutputStream outputStream = null;
         RandomAccessFile randomAccessFile = null;
         // 已传送数据大小
         long transmitted = 0;
         try {
-            randomAccessFile = new RandomAccessFile(music, "r");
+            randomAccessFile = new RandomAccessFile(downloadFile, "r");
             outputStream = new BufferedOutputStream(response.getOutputStream());
             byte[] buff = new byte[4096];
             int len = 0;
             randomAccessFile.seek(startByte);
-            // 坑爹地方四：判断是否到了最后不足4096（buff的length）个byte这个逻辑（(transmitted + len) <= contentLength）要放前面！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！
+            // ！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！
+            // 坑爹地方四：判断是否到了最后不足4096（buff的length）个byte这个逻辑（(transmitted + len) <= contentLength）要放前面
             // 不然会会先读取randomAccessFile，造成后面读取位置出错，找了一天才发现问题所在
             while ((transmitted + len) <= contentLength && (len = randomAccessFile.read(buff)) != -1) {
                 outputStream.write(buff, 0, len);
                 transmitted += len;
                 //TODO 停一下，方便测试，用的时候删了就行了
-                Thread.sleep(10);
+                // Thread.sleep(10);
             }
             // 处理不足buff.length部分
             if (transmitted < contentLength) {
@@ -159,9 +164,6 @@ public class FileController {
             e.printStackTrace();
         } catch (IOException e) {
             System.out.println("下载IOException: ");
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            System.out.println("下载InterruptedException: ");
             e.printStackTrace();
         } finally {
             try {
